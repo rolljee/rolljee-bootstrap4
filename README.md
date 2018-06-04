d797d01dc8b1a018e9fe32e0f344de5fdd45eab8
rolljee <n.riquelmebareiro@gmail.com>
2018-06-04 18:09:20 +0200

    Updated package to make it work!

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ I've taken the compiled version of Bootstrap4 and wrapped it inside a package.
 ```shell
 meteor add rolljee:bootstrap4
 ```
+
+## Documentation
+
+For the doc, click [here](https://getbootstrap.com/docs/4.0/getting-started/introduction/)
+
+## Issues
+
+Feel free to open issues if anything doesn't work as expected!
--- package.js
+++ package.js
@@ -1,13 +1,24 @@
 Package.describe({
-  name: 'rolljee:bootstrap4',
-  version: '0.0.1',
-  summary: 'bootstrap4 packaged for Meteorjs',
-  git: 'https://github.com/rolljee/rolljee-bootstrap4.git',
-  documentation: 'README.md'
+    name: 'rolljee:bootstrap4',
+    version: '0.0.2',
+    summary: 'bootstrap4 packaged for Meteorjs',
+    git: 'https://github.com/rolljee/rolljee-bootstrap4.git',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6.1.1');
-  api.use('ecmascript');
-  api.mainModule('bootstrap4.js');
+    api.addFiles([
+        './css/bootstrap-grid.css',
+        './css/bootstrap-reboot.css',
+        './css/bootstrap.css',
+        './js/bootstrap.js',
+    ], 'client');
+    api.use(['ecmascript']);
+    api.versionsFrom('1.6.1.1');
+    api.mainModule('bootstrap4.js');
+});
+
+Npm.depends({
+    "jquery": "3.3.1",
+    "popper.js": "1.14.3"
 });
